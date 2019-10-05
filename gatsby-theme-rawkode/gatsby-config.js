6b6dfe113b0a9ff38f66265c4cb97f0738ead59e
David McKay <david@rawkode.dev>
2019-10-05 04:46:26 -0700

    chore: disable Algolia for now (Build failing)

--- gatsby-theme-rawkode/gatsby-config.js
+++ gatsby-theme-rawkode/gatsby-config.js
@@ -75,15 +75,15 @@ module.exports = themeOptions => {
           icon: 'src/site-icon.png'
         }
       },
-      {
-        resolve: `gatsby-plugin-algolia`,
-        options: {
-          appId: process.env.GATSBY_ALGOLIA_APP_ID,
-          apiKey: process.env.ALGOLIA_ADMIN_KEY,
-          queries,
-          chunkSize: 10000
-        }
-      },
+      // {
+      //   resolve: `gatsby-plugin-algolia`,
+      //   options: {
+      //     appId: process.env.GATSBY_ALGOLIA_APP_ID,
+      //     apiKey: process.env.ALGOLIA_ADMIN_KEY,
+      //     queries,
+      //     chunkSize: 10000
+      //   }
+      // },
       {
         resolve: 'gatsby-plugin-google-tagmanager',
         options: {
