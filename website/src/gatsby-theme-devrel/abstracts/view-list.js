1f13a7e560106349830a372c08482369d0a4278f
David McKay <david@rawkode.dev>
2019-10-14 16:13:34 +0100

    chore: clean up headers on abstract list

--- website/src/gatsby-theme-devrel/abstracts/view-list.js
+++ website/src/gatsby-theme-devrel/abstracts/view-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Styled } from 'theme-ui';
 import Layout from 'gatsby-theme-rawkode/src/components/Layout';
 import ListItem from 'gatsby-theme-devrel/src/abstracts/view-list-item';
 
@@ -8,6 +9,19 @@ export default ({ location, data }) => {
 
   return (
     <Layout>
+      <Styled.h1>Speaking</Styled.h1>
+      <Styled.p>
+        I am more than happy to come and speak at your event. Please find a list of abstracts I have
+        available.
+      </Styled.p>
+
+      <Styled.p>
+        Even if one of the below doesnt pique your interest, get in touch and we can tailor
+        something unique to your audience.
+      </Styled.p>
+
+      <Styled.h2>Abstracts</Styled.h2>
+
       {allAbstract.edges.map(({ node }) => (
         <ListItem title={node.title} uri={node.slug} />
       ))}
